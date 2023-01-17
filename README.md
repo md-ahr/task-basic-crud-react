000d8d7fdb159e7e9c0d9c9e240a0f0504b37895
Abdul Halim <ahr.web.pro@gmail.com>
2023-01-17 23:09:24 +0600

    Initialize react app with vite and tailwind css

---
+++ README.md
@@ -0,0 +1,3 @@
+## Task - React App
+
+A simple form submit task using react and json server.
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="">Hello App</div>;
+}
+
+export default App;
